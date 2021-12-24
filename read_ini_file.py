90a0d64bff5d51177e9972a5f09786dad1743f8b
root <root@datavis-D620MT-D620SF-BM3CF>
2021-12-24 12:34:32 +0800

    fisrt commit

---
+++ read_ini_file.py
@@ -0,0 +1,18 @@
+from pyspark.sql import SparkSession
+import json
+
+spark = SparkSession.builder.master('local[*]').appName('spark_application').getOrCreate()
+
+df = spark.read.option('inferSchema', 'true').option("delimiter", ",").option('header', 'true').csv('./population_by_country_2020.csv')
+#df = spark.read.option("delimiter", ";").csv('./mbsa.csv')
+df.show(100)
+aaa = df.select("Country (or dependency)")
+
+listaaa = aaa.toJSON().collect()
+listdictaaa = []
+
+for e in listaaa:
+    listdictaaa.append(json.loads(e))
+
+print(listdictaaa[0]["Country (or dependency)"])
+#spark.stop()
